1ac25d8214200b9511749a24d73bf599d74e3a64
nglsnkrs <nglsnkrs@DESKTOP-6HM5FL4>
2025-06-27 23:40:24 +0300

    Shapes

---
+++ ShapesDrawing/ShapesDrawing/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ShapesDrawing
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rectangleButton = new System.Windows.Forms.Button();
+            this.circleButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // rectangleButton
+            // 
+            this.rectangleButton.Location = new System.Drawing.Point(12, 12);
+            this.rectangleButton.Name = "rectangleButton";
+            this.rectangleButton.Size = new System.Drawing.Size(100, 23);
+            this.rectangleButton.TabIndex = 0;
+            this.rectangleButton.Text = "Прямоугольник";
+            this.rectangleButton.UseVisualStyleBackColor = true;
+            this.rectangleButton.Click += new System.EventHandler(this.rectangleButton_Click);
+            this.rectangleButton.MouseHover += new System.EventHandler(this.rectangleButton_MouseHover);
+            // 
+            // circleButton
+            // 
+            this.circleButton.Location = new System.Drawing.Point(118, 12);
+            this.circleButton.Name = "circleButton";
+            this.circleButton.Size = new System.Drawing.Size(100, 23);
+            this.circleButton.TabIndex = 1;
+            this.circleButton.Text = "Круг";
+            this.circleButton.UseVisualStyleBackColor = true;
+            this.circleButton.Click += new System.EventHandler(this.circleButton_Click);
+            this.circleButton.MouseHover += new System.EventHandler(this.circleButton_MouseHover);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(224, 12);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 23);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.circleButton);
+            this.Controls.Add(this.rectangleButton);
+            this.Name = "MainForm";
+            this.Text = "Геометрические фигуры";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rectangleButton;
+        private System.Windows.Forms.Button circleButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ ShapesDrawing/ShapesDrawing/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ShapesDrawing {
+    public partial class MainForm : Form {
+        private string selectedShape = "Rectangle";
+        private bool isDrawing = false;
+        private Point startPoint;
+        private Point endPoint;
+        private Pen drawingPen = new Pen(Color.Black, 2);
+        private Bitmap drawingBitmap;
+        string strToolTip = "Щёлкните левой кнопкой мыши по форме и переместите указатель";
+        public MainForm() {
+            InitializeComponent();
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink; 
+            this.MaximizeBox = false; 
+            drawingBitmap = new Bitmap(this.ClientRectangle.Width, this.ClientRectangle.Height);
+            this.Paint += MainForm_Paint;
+            this.MouseDown += MainForm_MouseDown;
+            this.MouseMove += MainForm_MouseMove;
+            this.MouseUp += MainForm_MouseUp;
+        }
+        private void MainForm_Paint(object sender, PaintEventArgs e) {
+            e.Graphics.DrawImage(drawingBitmap, new Point(0, 0));
+        }
+        private void MainForm_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                isDrawing = true;
+                startPoint = new Point(e.X, e.Y);
+            }
+        }
+        private void MainForm_MouseMove(object sender, MouseEventArgs e) {
+            if (isDrawing) {
+                if (drawingBitmap != null) {
+                    using (Graphics g = Graphics.FromImage(drawingBitmap)) {
+                        g.Clear(Color.White);
+                        switch (selectedShape) {
+                            case "Rectangle":
+                                endPoint = new Point(e.X, e.Y);
+                                g.DrawRectangle(drawingPen, GetRectangle(startPoint, endPoint));
+                                break;
+                            case "Circle":
+                                endPoint = new Point(e.X, e.Y);
+                                g.DrawEllipse(drawingPen, GetRectangle(startPoint, endPoint));
+                                break;
+                        }
+                    }
+                    this.Invalidate();
+                }
+            }
+        }
+        private void MainForm_MouseUp(object sender, MouseEventArgs e) {
+            isDrawing = false;
+        }
+        private Rectangle GetRectangle(Point p1, Point p2) {
+            Rectangle rect = new Rectangle();
+            rect.X = Math.Min(p1.X, p2.X);
+            rect.Y = Math.Min(p1.Y, p2.Y);
+            rect.Width = Math.Abs(p1.X - p2.X);
+            rect.Height = Math.Abs(p1.Y - p2.Y);
+            return rect;
+        }
+        private void rectangleButton_Click(object sender, EventArgs e) {
+            selectedShape = "Rectangle";
+        }
+        private void circleButton_Click(object sender, EventArgs e) {
+            selectedShape = "Circle";
+        }
+        private void clearButton_Click(object sender, EventArgs e) {
+            if (drawingBitmap != null) {
+                using (Graphics g = Graphics.FromImage(drawingBitmap)) {
+                    g.Clear(Color.White);
+                    this.Invalidate();
+                }
+            }
+        }
+        private void saveButton_Click(object sender, EventArgs e) {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Bitmap Image|*.bmp|JPEG Image|*.jpg";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK) {
+                string fileName = saveFileDialog.FileName;
+                switch (System.IO.Path.GetExtension(fileName).ToLower()) {
+                    case ".bmp":
+                        drawingBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case ".jpg":
+                        drawingBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    default:
+                        MessageBox.Show("Unsupported file format");
+                        break;
+                }
+            }
+        }
+        private void rectangleButton_MouseHover(object sender, EventArgs e) {
+            toolTip1.SetToolTip(rectangleButton, strToolTip);
+        }
+        private void circleButton_MouseHover(object sender, EventArgs e) {
+            toolTip1.SetToolTip(circleButton, strToolTip);
+        }
+    }
+}
+
